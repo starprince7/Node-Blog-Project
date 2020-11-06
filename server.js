9bffc76ebe291e83960ba80f34c5c8479cf82584
starprince7 <princeagezinweke@gmail.com>
2020-11-06 14:39:00 +0100

    version 1.0.0

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const authRouter = require('./routes/authRoutes')
 const cookieParser = require('cookie-parser')
 const { checkUser } = require('./middlewares/auth')
 
-require('dotenv').config();
 
 const app = express();
 
@@ -23,7 +22,7 @@ app.set('view engine', 'ejs')
 // port 
 const port = process.env.PORT || 4000
 // db connection 
-const dbURI = `mongodb+srv://starprince:${process.env.DB_PASSWORD}@starprince.m9v4i.mongodb.net/Projects?retryWrites=true&w=majority`
+const dbURI = `mongodb+srv://starprince:starprince7@starprince.m9v4i.mongodb.net/Projects?retryWrites=true&w=majority`
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, autoIndex: true })
     .then(result => {
         console.log('Connected to the Database!...')
